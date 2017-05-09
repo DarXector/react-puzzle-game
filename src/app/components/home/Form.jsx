660d656ab8bdaf675d3b26b84a4be0312f4a86e2
Reesta <marko.reestic@gmail.com>
2017-05-09 23:23:46 +0200

    Form moved from Home.jsx into Form.jsx

---
+++ src/app/components/home/Form.jsx
@@ -0,0 +1,97 @@
+import React, {Component} from 'react';
+import TextInput from '../common/input/TextInput';
+
+class Form extends Component {
+
+    commonValidate() {
+        return true;
+    }
+
+    validateEmail(value) {
+        var re = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+        return re.test(value);
+    }
+
+    validatePhone(value) {
+        var re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+        return re.test(value);
+    }
+
+    render() {
+
+        const {userID, userUpdate, onConfirm} = this.props;
+
+        return (
+            <div className={ `form ${userID? '' : 'hidden'}` }>
+                <label>IME I PREZIME</label>
+                <TextInput
+                    className="input-holder"
+                    invalidClass="input-error"
+                    text="Janko Marko"
+                    required={true}
+                    minCharacters={6}
+                    validate={ this.commonValidate }
+                    onChange={ value => userUpdate({prop: 'name', value}) }
+                    errorMessage="Ime nije validno"
+                    emptyMessage="Ime je obavezno"
+                    ref={(input) => { this.nameInput = input }} />
+
+                <label>E-MAIL</label>
+                <TextInput
+                    className="input-holder"
+                    invalidClass="input-error"
+                    text="mail@mail.com"
+                    required={true}
+                    minCharacters={6}
+                    validate={ this.validateEmail }
+                    onChange={ value => userUpdate({prop: 'email', value}) }
+                    errorMessage="Email nije validan"
+                    emptyMessage="Email je obavezan"
+                    ref={(input) => { this.emailInput = input }} />
+
+                <label> GRAD I<br/>POŠTANSKI BROJ</label>
+                <TextInput
+                    className="input-holder"
+                    invalidClass="input-error"
+                    text="Zagreb"
+                    required={true}
+                    minCharacters={2}
+                    validate={ this.commonValidate }
+                    onChange={ value => userUpdate({prop: 'city', value}) }
+                    errorMessage="Grad nije validan"
+                    emptyMessage="Grad je obavezan"
+                    ref={(input) => { this.cityInput = input }} />
+
+                <label>DRŽAVA</label>
+                <TextInput
+                    className="input-holder"
+                    invalidClass="input-error"
+                    text="Hrvatska"
+                    required={true}
+                    minCharacters={3}
+                    validate={ this.commonValidate }
+                    onChange={ value => userUpdate({prop: 'country', value}) }
+                    errorMessage="Država nije validna"
+                    emptyMessage="Država je obavezna"
+                    ref={(input) => { this.countryInput = input }} />
+
+                <label>BROJ MOBITELA</label>
+                <TextInput
+                    className="input-holder"
+                    invalidClass="input-error"
+                    text="+3XXXXXXXXXXX"
+                    required={true}
+                    minCharacters={6}
+                    validate={ this.validatePhone }
+                    onChange={ value => userUpdate({prop: 'phone', value}) }
+                    errorMessage="Broj mobitela nije validan"
+                    emptyMessage="Broj mobitela je obavezan"
+                    ref={(input) => { this.phoneInput = input }} />
+
+                <a onClick={ onConfirm }>POTVRDI</a>
+            </div>
+        )
+    }
+}
+
+export default Form;
--- src/app/components/home/Home.jsx
+++ src/app/components/home/Home.jsx
@@ -5,7 +5,7 @@ import _ from 'lodash';
 import { browserHistory } from 'react-router'
 
 import Page from '../common/Page'
-import TextInput from '../common/input/TextInput';
+import Form from './Form';
 
 import { userSave, userUpdate, userCheckRegistered } from '../../actions/'
 
@@ -15,7 +15,7 @@ class Home extends Component {
         super();
         this.inputs = [];
         this.state = {
-            userID: false
+            userID: ''
         }
     }
 
@@ -30,10 +30,10 @@ class Home extends Component {
                 if(this.props.loggedIn) {
                     browserHistory.push('/pregame');
                 } else {
-                    this.nameInput.validation(name, true);
+                    this.form.nameInput.validation(name, true);
                     this.props.userUpdate({prop: 'name', value: name});
 
-                    this.emailInput.validation(email, true);
+                    this.form.emailInput.validation(email, true);
                     this.props.userUpdate({prop: 'email', value: email});
 
                     this.setState({ userID });
@@ -42,22 +42,14 @@ class Home extends Component {
         }
     }
 
-    commonValidate() {
-        return true;
-    }
-
-    validateEmail(value) {
-        var re = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-        return re.test(value);
-    }
-
-    validatePhone(value) {
-        var re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
-        return re.test(value);
-    }
-
     onConfirm() {
-        this.inputs = [this.nameInput, this.emailInput, this.countryInput, this.cityInput, this.phoneInput];
+        this.inputs = [
+            this.form.nameInput,
+            this.form.emailInput,
+            this.form.countryInput,
+            this.form.cityInput,
+            this.form.phoneInput
+        ];
 
         var allValid = _.every(this.inputs, (input)=>
         {
@@ -83,74 +75,10 @@ class Home extends Component {
 
                 <p className="title">PODIJELI SA NAMA NEKOLIKO INFORMACIJA</p>
 
-                <div className={ `form ${this.state.userID? '' : 'hidden'}` }>
-                    <label>IME I PREZIME</label>
-                    <TextInput
-                        className="input-holder"
-                        invalidClass="input-error"
-                        text="Janko Marko"
-                        required={true}
-                        minCharacters={6}
-                        validate={ this.commonValidate }
-                        onChange={ value => this.props.userUpdate({prop: 'name', value}) }
-                        errorMessage="Ime nije validno"
-                        emptyMessage="Ime je obavezno"
-                        ref={(input) => { this.nameInput = input }} />
-
-                    <label>E-MAIL</label>
-                    <TextInput
-                        className="input-holder"
-                        invalidClass="input-error"
-                        text="mail@mail.com"
-                        required={true}
-                        minCharacters={6}
-                        validate={ this.validateEmail }
-                        onChange={ value => this.props.userUpdate({prop: 'email', value}) }
-                        errorMessage="Email nije validan"
-                        emptyMessage="Email je obavezan"
-                        ref={(input) => { this.emailInput = input }} />
-
-                    <label> GRAD I<br/>POŠTANSKI BROJ</label>
-                    <TextInput
-                        className="input-holder"
-                        invalidClass="input-error"
-                        text="Zagreb"
-                        required={true}
-                        minCharacters={2}
-                        validate={ this.commonValidate }
-                        onChange={ value => this.props.userUpdate({prop: 'city', value}) }
-                        errorMessage="Grad nije validan"
-                        emptyMessage="Grad je obavezan"
-                        ref={(input) => { this.cityInput = input }} />
-
-                    <label>DRŽAVA</label>
-                    <TextInput
-                        className="input-holder"
-                        invalidClass="input-error"
-                        text="Hrvatska"
-                        required={true}
-                        minCharacters={3}
-                        validate={ this.commonValidate }
-                        onChange={ value => this.props.userUpdate({prop: 'country', value}) }
-                        errorMessage="Država nije validna"
-                        emptyMessage="Država je obavezna"
-                        ref={(input) => { this.countryInput = input }} />
-
-                    <label>BROJ MOBITELA</label>
-                    <TextInput
-                        className="input-holder"
-                        invalidClass="input-error"
-                        text="+3XXXXXXXXXXX"
-                        required={true}
-                        minCharacters={6}
-                        validate={ this.validatePhone }
-                        onChange={ value => this.props.userUpdate({prop: 'phone', value}) }
-                        errorMessage="Broj mobitela nije validan"
-                        emptyMessage="Broj mobitela je obavezan"
-                        ref={(input) => { this.phoneInput = input }} />
-
-                    <a onClick={this.onConfirm.bind(this)}>POTVRDI</a>
-                </div>
+                <Form userID={ this.state.userID }
+                      userUpdate={ this.props.userUpdate }
+                      onConfirm={ this.onConfirm.bind(this) }
+                      ref={(input) => { this.form = input }}/>
 
                 {this.state.userID? '' : <FacebookLogin
                     textButton="POVEŽI SA FACEBOOKOM"
